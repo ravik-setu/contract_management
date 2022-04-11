c1b0c2d37b0524198dc1c144a16e63a60ad01c4c
savaniyajigna <jigna.setuconsulting@gmail.com>
2022-04-11 14:52:35 +0530

    [JS][add] added payment and timesheet to show timesheet and payments

--- contract_management_setu/models/account_move.py
+++ contract_management_setu/models/account_move.py
@@ -5,3 +5,4 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     project_id = fields.Many2one("project.project", string="Project")
+    contract_id = fields.Many2one("hr.contract", string="Contract")
--- contract_management_setu/models/contract_management.py
+++ contract_management_setu/models/contract_management.py
@@ -18,7 +18,9 @@ class HrContract(models.Model):
     invoice_count = fields.Integer(string='Invoice Count', compute='_compute_invoice_count')
     description = fields.Html(string="Description")
     is_maintain_timesheet = fields.Boolean(string="Maintain a timesheet ?")
-    timesheet_ids = fields.One2many("account.analytic.line", 'contract_id', string="Timesheet")
+    # timesheet_ids = fields.One2many("account.analytic.line", 'contract_id', string="Timesheet")
+    payment_count = fields.Integer(string="Payment Count", compute='_compute_payment_count')
+    timesheet_count = fields.Integer(string="Timesheet Count", compute='compute_timesheet')
 
     @api.depends('contract_uom', 'hours_per_day', 'contract_quantity')
     def _compute_total_contract_service_hours(self):
@@ -42,17 +44,86 @@ class HrContract(models.Model):
         """
         action = self.env["ir.actions.actions"]._for_xml_id("account.action_move_out_invoice_type")
         domain = eval(action['domain'])
-        invoice_ids = self.env['account.move'].search([('project_id', '=', self.project_id.id)])
-        domain.append(('id', 'in', invoice_ids.ids))
+        domain.append(('id', 'in', self.get_project_and_contract_invoice()))
+        context = {
+            'default_move_type': 'out_invoice',
+        }
+        if len(self) == 1:
+            context.update({
+                'default_partner_id': self.partner_id.id,
+                'default_project_id': self.project_id.id,
+                'default_contract_id': self.id,
+            })
+        action['context'] = context
         action['domain'] = domain
         return action
 
-    @api.depends('project_id.invoice_ids')
     def _compute_invoice_count(self):
         """
         Added By: Jigna J Savaniya | Date: 7th April,2022 | Task : 600
         Use: This method is used to count invoices as per project
         """
         for contract in self:
-            invoices = contract.project_id.invoice_ids.filtered(lambda r: r.move_type in ('out_invoice', 'out_refund'))
-            contract.invoice_count = len(invoices)
+            contract.invoice_count = len(self.get_project_and_contract_invoice())
+
+    def get_project_and_contract_invoice(self):
+        """
+        Added By: Jigna J Savaniya | Date: 9th April,2022 | Task : 600
+        Use: This method is used to get invoice ids based on project and contract
+        """
+        return self.env['account.move'].search(
+            [('project_id', '=', self.project_id.id), ('contract_id', '=', self.id)]).ids
+
+    def action_view_customer_payment(self):
+        """
+        Added By: Jigna J Savaniya | Date: 9th April,2022 | Task : 600
+        Use: This method is used to show payments of invoices based on contract invoice
+        """
+        action = self.env["ir.actions.actions"]._for_xml_id("account.action_account_payments")
+        move_ids = self.get_payment_of_invoices()
+        action['domain'] = [('move_id', 'in', move_ids)]
+        return action
+
+    def _compute_payment_count(self):
+        """
+        Added By: Jigna J Savaniya | Date: 9th April,2022 | Task : 600
+        Use: This method is used to calculate payments of invoices based on contract invoice
+        """
+        move_ids = self.get_payment_of_invoices()
+        self.payment_count = len(move_ids)
+
+    def get_payment_of_invoices(self):
+        """
+        Added By: Jigna J Savaniya | Date: 9th April,2022 | Task : 600
+        Use: This method is used to get move ids based on contract
+        """
+        invoices = self.env['account.move'].search([('contract_id', '=', self.id)])
+        move_ids = []
+        for move in invoices:
+            for partial, amount, counterpart_line in move._get_reconciled_invoices_partials():
+                data = (move._get_reconciled_vals(partial, amount, counterpart_line))
+                data and move_ids.append(data.get('move_id'))
+        return move_ids
+
+    def action_view_time_sheet(self):
+        """
+        Added By: Jigna J Savaniya | Date: 11th April,2022 | Task : 600
+        Use: This method is used to show timesheet as per contract
+        """
+        action = self.env["ir.actions.actions"]._for_xml_id("hr_timesheet.act_hr_timesheet_line")
+        action['domain'] = [('id', 'in', self.get_timesheet_of_contract())]
+        return action
+
+    def compute_timesheet(self):
+        """
+        Added By: Jigna J Savaniya | Date: 11th April,2022 | Task : 600
+        Use: This method is used to calculate timesheet as per contract
+        """
+        self.timesheet_count = len(self.get_timesheet_of_contract())
+
+    def get_timesheet_of_contract(self):
+        """
+        Added By: Jigna J Savaniya | Date: 11th April,2022 | Task : 600
+        Use: This method is used to get timesheet ids based on contract
+        """
+        return self.env['account.analytic.line'].search([('contract_id', '=', self.id)]).ids
