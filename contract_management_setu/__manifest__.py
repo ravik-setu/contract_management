aa6c1c1028d728fd4a59ce05a7f59d793ec8f7c0
savaniyajigna <jigna.setuconsulting@gmail.com>
2022-04-12 11:20:34 +0530

    [JS][Add] added contract utilization report

--- contract_management_setu/__manifest__.py
+++ contract_management_setu/__manifest__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Contract Management',
+    'name': 'Contract Management System',
     'version': '15.0',
     'category': 'Accounting',
+    'description': 'Contract Management System',
     'website': 'https://www.odoo.com/app/invoicing',
     'depends': ['documents_hr_contract', 'timesheet_grid', 'project', 'account'],
     'data': [
@@ -11,6 +12,7 @@
         'views/project_project_view.xml',
         'views/account_move_view.xml',
         'views/res_partner_view.xml',
+        'views/contract_utilization_report.xml'
     ],
     'demo': [
     ],
